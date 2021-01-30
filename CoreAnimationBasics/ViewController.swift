263ac9444868cb339c2e78c9b9040b4ae3648998
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-30 21:35:47 +0200

    init

--- CoreAnimationBasics/ViewController.swift
+++ CoreAnimationBasics/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let redView = UIView()
+    let _width: CGFloat = 140
+    let _height: CGFloat = 100
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.backgroundColor = .systemRed
+        
+        view.addSubview(redView)
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        redView.frame = CGRect(x: view.bounds.midX - _width/2,
+                               y: view.bounds.midY - _height/2,
+                               width: _width, height: _height)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            self.animate()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.rotate()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.shake()
+        }
+    }
+    
+    func animate() {
+        let animation = CABasicAnimation()
+        animation.keyPath = "transform.scale"
+        animation.fromValue = 1
+        animation.toValue = 2
+        animation.duration = 0.4
+        
+        redView.layer.add(animation, forKey: "basic")
+        redView.layer.transform = CATransform3DMakeScale(2, 2, 1)
+    }
+    
+    func rotate() {
+        let animation = CABasicAnimation()
+        animation.keyPath = "transform.rotation.z"
+        animation.fromValue = 0
+        animation.toValue = CGFloat.pi / 4
+        animation.duration = 1
+        
+        redView.layer.add(animation, forKey: "basic")
+        redView.layer.transform = CATransform3DMakeRotation(CGFloat.pi / 4, 0, 0, 1)
+    }
+    
+    func shake() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        redView.layer.add(animation, forKey: "shake")
+    }
 
 }
 
